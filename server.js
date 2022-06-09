943243f680e0d792beb0e250ab7df7bf6a6ac0cd
Lisa Kilbinger <lizkil.dev@gmail.com>
2022-06-09 14:29:26 +0200

    fixed sec probs

--- server.js
+++ server.js
@@ -4,79 +4,93 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const fs = require('fs');
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
-const connectionString = 'mongodb+srv://lizkil:{L30nN03l}@cluster0.5xevu.mongodb.net/?retryWrites=true&w=majority'
+const connectionString = 'mongodb+srv://@cluster0.5xevu.mongodb.net/?retryWrites=true&w=majority'
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/lizkil-rick-and-morty-quotes');
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/lizkil-rick-and-morty-quotes');
+new Promise((res, rej) => {
 
-MongoClient.connect(connectionString, { useUnifiedTopology: true })
+  fs.readFile(
+    './credentials.json',
+    (err, data) => {
+      if (err) {
+        rej(err);
+      } else {
+        res(JSON.parse(data.toString()));
+      }
+    })
+})
+  .then(
+    (data) => MongoClient.connect(`mongodb+srv://${data.user}:${data.pw}@cluster0.5xevu.mongodb.net/?retryWrites=true&w=majority`, { useUnifiedTopology: true })
+  )
   .then(client => {
     console.log('Connected to Database');
     const db = client.db('rickandmorty-quotes');
     const quotesCollection = db.collection('quotes');
     app.set('view engine', 'ejs');
-    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.urlencoded({ extended: true }));
     app.use('/public', express.static('public'));
     app.use(bodyParser.json());
-    
+
 
     //Create (POST)
     app.post('/quotes', (req, res) => {
       quotesCollection.insertOne(req.body)
-      .then(result => {
-        // console.log(result);
-        res.redirect('/')
-      })
-      .catch(error => console.error(error))
-      })
-    
+        .then(result => {
+          // console.log(result);
+          res.redirect('/')
+        })
+        .catch(error => console.error(error))
+    })
+
     //Read (GET)
     app.get('/', (req, res) => {
       quotesCollection.find().toArray()
         .then(results => {
           // console.log(results);
-          res.render('index.ejs', {quotes: results})
+          res.render('index.ejs', { quotes: results })
         })
         .catch(error => console.error(error));
-    })  
-    
+    })
+
     //Update (PUT)
     app.put('/quotes', (req, res) => {
       quotesCollection.findOneAndUpdate(
-        {name: 'Morty'}, 
+        { name: 'Morty' },
         {
-            $set: {
-              name: req.body.name,
-              quote: req.body.quote
-            }
+          $set: {
+            name: req.body.name,
+            quote: req.body.quote
+          }
         },
         {
           upsert: true
         }
       )
-      .then(result => {
-        console.log(result);
-        res.json('Success')
-      })
-      .catch(error => console.error(error))
+        .then(result => {
+          console.log(result);
+          res.json('Success')
+        })
+        .catch(error => console.error(error))
     })
 
     //Delete (DELETE)
     app.delete('/quotes', (req, res) => {
       quotesCollection.deleteOne(
-        {name: req.body.name},
+        { name: req.body.name },
       )
-      .then(result => {
-        res.json('Deleted Rick\'s Quote')
-      })
-      .catch(error => console.error(error))
+        .then(result => {
+          res.json('Deleted Rick\'s Quote')
+        })
+        .catch(error => console.error(error))
     })
 
     //server
-    app.listen(process.env.PORT || 3000, function() {
-    console.log('listening on 3000')
-    })       
+    app.listen(process.env.PORT || 3000, function () {
+      console.log('listening on 3000')
+    })
     module.exports = app;
-})
-.catch(error => console.error(error))
+  })
+  .catch(error => console.error(error))
